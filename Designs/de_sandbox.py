e34432dd99527e07bf71b0e10c4c958eae6ff59f
Jonathan Balat <engrjonathanbalat@gmail.com>
2023-12-17 20:54:49 +0800

    Cleanup of files in master, to make way of creating dev and subbranches

--- Designs/de_sandbox.py
+++ Designs/de_sandbox.py
@@ -1,6 +1,5 @@
-from PySide2.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsItem
-# from Designs.Objects.Rect_obj import RectObj
-from Designs.Objects.RectObj2 import RectObj
+from PySide2.QtWidgets import QGraphicsScene, QGraphicsView
+from Designs.Objects.Obj_Rect import RectObj
 
 
 class SandBox(QGraphicsScene):
@@ -13,7 +12,6 @@ class SandBox(QGraphicsScene):
     # def mousePressEvent(self, event):
     #     print(event.buttonDownScenePos(QtCore.Qt.MouseButton.LeftButton))  # Widget-screen position
     #     print(event.buttonDownScreenPos(QtCore.Qt.MouseButton.LeftButton))  # Absolute screen position
-
 
     def add_rect(self, x, y, w, h):
         self.addItem(RectObj(self, x, y, w, h))
